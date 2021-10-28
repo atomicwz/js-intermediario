1122b4d8b3a293c97651d1b18c021e0dfa0627c0
Dener Oliveira <78823732+atomicwz@users.noreply.github.com>
2021-10-28 12:20:33 -0300

    final.

--- js-intermediario-quest-1/script.js
+++ js-intermediario-quest-1/script.js
@@ -2,18 +2,18 @@ const slide = document.querySelectorAll('.verificar')
 const test = document.querySelectorAll('.slide')
 const btnNext = document.querySelector('.next')
 const btnBack = document.querySelector('.back')
-let max = slide.length - 1
+let max = slide.length
 let i = 0
 
 function nextSlide(){
-
+    if(i >= max) return;
     btnBack.style.opacity = '100%'
     
     slide[i].classList.add('ativo')
     
     
     
-    if(i >= max){
+    if(i >= max - 1){
         btnNext.style.opacity = '20%'
     }
 
@@ -22,14 +22,19 @@ function nextSlide(){
 
 const button = document.querySelectorAll('button')
 
-
-
 function backSlide(){
+    if(i <= 0) return;
     i--   
     slide[i].classList.remove('ativo')
     btnNext.style.opacity = '100%'
+
+    if(i == 0){
+
+        btnBack.style.opacity = '20%'
+    }
     
 }
+
 button[1].addEventListener('click', nextSlide)
 button[0].addEventListener('click', backSlide)
 
